6f14f01fb6359ead2b26288cdee35b80a3a7581f
wiisportsreort <devdoge1@gmail.com>
2021-11-01 13:04:36 -0400

    add operator lab

---
+++ 2021-11-01-operator-lab/main.py
@@ -0,0 +1,24 @@
+# input number of hours, minutes, and seconds
+hours = int(input("Enter the number of hours: "))
+minutes = int(input("Enter the number of minutes: "))
+seconds = int(input("Enter the number of seconds: "))
+
+# input output format: hours, minutes, or seconds
+format = input(
+    "Enter the format you want to output (hours, minutes, seconds): "
+)
+
+if format == "hours":
+    result = hours + (minutes / 60) + (seconds / 3600)
+elif format == "minutes":
+    result = hours * 60 + minutes + (seconds / 60)
+elif format == "seconds":
+    result = hours * 3600 + minutes * 60 + seconds
+else:
+    print("Invalid format")
+    exit()
+
+print(f'{format.capitalize()}: {result}')
+
+
+# thank you copilot
