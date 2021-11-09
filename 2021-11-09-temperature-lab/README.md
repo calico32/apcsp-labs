2d59238f0f675fff33fadc0caeff4f75442fca11
wiisportsreort <devdoge1@gmail.com>
2021-11-09 13:20:27 -0500

    add temperature lab

---
+++ 2021-11-09-temperature-lab/README.md
@@ -0,0 +1,17 @@
+# Temperature Lab
+
+function
+
+`main.py`
+
+```
+❯ python main.py
+Enter source temperature (e.g. 32f, 16.3C): 123c
+123.0°C is equal to 253.4°F
+```
+
+```
+❯ python main.py
+Enter source temperature (e.g. 32f, 16.3C): 342F
+342.0°F is equal to 172.22°C
+```
---
+++ 2021-11-09-temperature-lab/main.py
@@ -0,0 +1,34 @@
+# convert f to c
+def f_to_c(f: float):
+    c = (f - 32) * 5 / 9
+    return c
+
+
+# convert c to f
+def c_to_f(c: float):
+    f = c * 9 / 5 + 32
+    return f
+
+
+if __name__ == '__main__':
+    source: str = ''
+    while not source:
+        source = input("Enter source temperature (e.g. 32f, 16.3C): ").lower()
+
+    try:
+        if source.endswith('f'):
+            value = float(source[:-1])
+            source = f'{value}°F'
+            result = f'{round(f_to_c(value), 2)}°C'
+        elif source.endswith('c'):
+            value = float(source[:-1])
+            source = f'{value}°C'
+            result = f'{round(c_to_f(value), 2)}°F'
+        else:
+            print('Invalid unit')
+            exit()
+    except ValueError:
+        print("Invalid number")
+        exit()
+
+    print(f'{source} is equal to {result}')
