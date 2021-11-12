930c541f3b14ee9c77d6021dcd3fd0367ab5a5c7
wiisportsreort <devdoge1@gmail.com>
2021-11-12 11:59:59 -0500

    add surface area lab

---
+++ 2021-11-11-surface-area-lab/README.md
@@ -0,0 +1,11 @@
+# Surface Area Lab
+
+`python main.py`
+
+```
+❯ python 2021-11-11-surface-area-lab/main.py
+Enter side 1 length: 123
+Enter side 2 length: 123
+Enter side 3 length: 123
+Surface area: 90774.0 units
+```
---
+++ 2021-11-11-surface-area-lab/main.py
@@ -0,0 +1,13 @@
+def area(s1: float, s2: float) -> float:
+    return s1 * s2
+
+
+def surface_area(s1: float, s2: float, s3: float) -> float:
+    return 2 * (area(s1, s2) + area(s2, s3) + area(s1, s3))
+
+
+if __name__ == '__main__':
+    s1 = float(input("Enter side 1 length: "))
+    s2 = float(input("Enter side 2 length: "))
+    s3 = float(input("Enter side 3 length: "))
+    print(f"Surface area: {surface_area(s1, s2, s3)} units")
